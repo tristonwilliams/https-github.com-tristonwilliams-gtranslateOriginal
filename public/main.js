de598fb3d3c485ce24d5c030b819fdd2e77d409a
Deepak John <deepakjohn@outlook.com>
2018-05-09 22:17:32 +0100

    Clean up

--- public/main.js
+++ public/main.js
@@ -4,43 +4,45 @@ let sdk = new window.sfdc.BlockSDK(); //initalize SDK
 let defaultContent = `<h1>This is the defualt content</h1>`;
 
 let data = {
-    text: '',
-    from: 'auto',
-    to: 'en'
+    textMessage: '',
+    fromLang: 'auto',
+    toLang: 'en'
 };
 
 let saveData = () => {
     console.log('Saving data...');
 
-
-    let textMessage = document.getElementById('textMessage').value;
-    let fromLang = document.getElementById('fromLang').value;
-    let toLang = document.getElementById('toLang').value;
+    data.textMessage = document.getElementById('textMessage').value;
+    data.fromLang = document.getElementById('fromLang').value;
+    data.toLang = document.getElementById('toLang').value;
 
     let xhttp = new XMLHttpRequest();
 
     xhttp.open('POST', '/translate', true);
     xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    xhttp.send(`text=${textMessage}&from=${fromLang}&to=${toLang}`);
+    xhttp.send(`text=${data.textMessage}&from=${data.fromLang}&to=${data.toLang}`);
 
     xhttp.onreadystatechange = () => {
         console.log('response -> ' + xhttp.responseText);
-        document.getElementById('t1').innerText = xhttp.responseText;
+        // document.getElementById('t1').innerText = xhttp.responseText;
+
+        sdk.setData(data, (updatedData) => {
+            let content = xhttp.responseText;
+            sdk.setContent(content);
+        });
     }
 
-    // sdk.setData(newData, (updatedData) => {
-    //     let content = `Content to change`;
-
-    //     sdk.setContent(content);
-    // });
 }
 
 let fetchData = () => {
     console.log('Loading data...');
 
-
     sdk.getData((data) => {
         if (Object.keys(data).length > 0) {
+
+            document.getElementById('textMessage').value = data.text;
+            document.getElementById('fromLang').value = data.from;
+            document.getElementById('toLang').value = data.to;
 
             // console.log('Found data!');
         }
